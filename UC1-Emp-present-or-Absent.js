f783d950e3afd7dba2cc1679710c3e10938da729
rohit789@ <princeshmr27@gmail.com>
2023-09-13 20:43:50 +0530

    Check Employee is Present Or Absent

---
+++ UC1-Emp-present-or-Absent.js
@@ -0,0 +1,11 @@
+console.log("Welcome to Employee Payroll");
+
+//Uc-1 Check Employee is Present Or Absent
+
+let empCheck=Math.floor(Math.random()*10)%3;
+let IS_ABSENT=0;
+if(empCheck==IS_ABSENT){
+    console.log("Employee is Absent");
+}else{
+    console.log("Employee is Present");
+}
